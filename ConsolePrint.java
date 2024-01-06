1461ce63ba0165d600cb4cac42672fe0a10fc6da
heedii <127703762+heedii@users.noreply.github.com>
2024-01-06 02:30:55 -0800

    Update ConsolePrint.java

--- ConsolePrint.java
+++ ConsolePrint.java
@@ -1,5 +1,6 @@
 public class ConsolePrint {
     public static void main(String[] args) { // Where the execution begins
         System.out.println("Hello, I'm Heedi"); // What the program says
+        
     }
 }
